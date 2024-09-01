3f0bf7b869d02dae5b8c4858f3edb440d037f1d6
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-01 22:05:54 +0430

    add carousel component

--- app/_tabs/index.js
+++ app/_tabs/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import { Dimensions } from "react-native";
 import { View, Text, StyleSheet } from "react-native";
+import Carousel from "../components/Carousel";
+sampleData = [
+  { id: 1, title: "test" },
+  { id: 2, title: "test" },
+  { id: 3, title: "test" },
+];
+
+const renderItem=({item})=>(
+  <View style={styles.item}>
+    <Text>{item.title}</Text>
+  </View>
+)
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello from Homddde</Text>
+      <Carousel data={sampleData} renderItem={renderItem}/>
     </View>
   );
 };
@@ -20,4 +34,15 @@ const styles = StyleSheet.create({
     fontSize: 24,
     fontWeight: "700",
   },
+  item:{
+    width: Dimensions.get("window").width*0.75,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#e0e0e0",
+    borderRadius: 8,
+    padding: 20,
+    marginHorizontal: 8,
+    height: Dimensions.get("window").width*0.75
+  },
+  
 });
