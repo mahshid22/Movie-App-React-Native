7d18ad4a5f3596623345e0098d5d1ddab3afb237
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-02 21:14:08 +0430

    them context

---
+++ context/ThemeContext.js
@@ -0,0 +1,30 @@
+// ThemeContext.js
+import React, { createContext, useContext, useState, useMemo } from 'react';
+import { Provider as PaperProvider, DefaultTheme, DarkTheme } from 'react-native-paper';
+import { useColorScheme } from 'react-native';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const systemTheme = useColorScheme();
+  const [theme, setTheme] = useState(systemTheme === 'dark' ? 'dark' : 'light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'dark' ? 'light' : 'dark'));
+  };
+
+  const paperTheme = useMemo(
+    () => (theme === 'dark' ? DarkTheme : DefaultTheme),
+    [theme]
+  );
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <PaperProvider theme={paperTheme}>
+        {children}
+      </PaperProvider>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
