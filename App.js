7a0e1609741ba2964002ea5377f6e1249bc02cf4
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-02 21:12:51 +0430

    add theme provider

--- App.js
+++ App.js
@@ -2,16 +2,16 @@ import { ExpoRoot } from "expo-router";
 import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 // import { UserProvider } from './context/UserContext';
-// import { ThemeProvider } from './context/ThemeContext';
+import { ThemeProvider } from './context/ThemeContext';
 
 export default function App() {
   const ctx = require.context("./app/_tabs");
 
   return (
     // <UserProvider>
-    //   <ThemeProvider>
+      <ThemeProvider>
         <ExpoRoot context={ctx} />
-    //   </ThemeProvider>
+      </ThemeProvider>
     // </UserProvider>
   );
 }
