394b5a81fd6b9991b9e1b3b467b36697fcb8123c
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-02 21:14:25 +0430

    implement tv show and movie card

--- app/_tabs/index.js
+++ app/_tabs/index.js
@@ -2,22 +2,48 @@ import React from "react";
 import { Dimensions } from "react-native";
 import { View, Text, StyleSheet } from "react-native";
 import Carousel from "../components/Carousel";
-sampleData = [
-  { id: 1, title: "test" },
-  { id: 2, title: "test" },
-  { id: 3, title: "test" },
-];
+import MovieCard from "../components/MovieCard";
+import TVShowCard from "../components/TVShowCard";
 
-const renderItem=({item})=>(
-  <View style={styles.item}>
-    <Text>{item.title}</Text>
-  </View>
+sampleData =[
+  {
+    id:1,
+    title: "Star Wars: Episode IV - A New Hope",
+    
+    poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
+    Ratings: "8"
+  },
+  {
+    id:2,
+    title: "B",
+    
+    poster: "https://m.media-amazon.com/images/M/MV5BZDhlZmRlNmMtYmMyYy00Zjg0LWI0ZmQtYzNiNWM5YTU4YTI3XkEyXkFqcGdeQXVyNjQ0NjY3MDY@._V1_SX300.jpg",
+    Ratings: "8"
+
+  },
+  {
+    id:3,
+    title: "C",
+    poster: "https://m.media-amazon.com/images/M/MV5BOTM5Njc5ZTYtYzk1OS00ZmIxLTlkOTAtZmE3MjBiNjQ4MWQyXkEyXkFqcGdeQXVyNjIyNDgwMzM@._V1_SX300.jpg",
+    Ratings: "8"
+
+  }
+]
+
+const renderMovieItem=({item})=>(
+  <MovieCard movie={item}/>
+)
+const renderTVShowItem=({item})=>(
+  <TVShowCard show={item}/>
 )
 const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Hello from Homddde</Text>
-      <Carousel data={sampleData} renderItem={renderItem}/>
+      <Text style={styles.text}>Movies:</Text>
+      <Carousel data={sampleData} renderItem={renderMovieItem}/>
+      <Text style={styles.text}>TV shows:</Text>
+      <Carousel data={sampleData} renderItem={renderTVShowItem}/>
     </View>
   );
 };
@@ -29,6 +55,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 10,
+    marginTop: 50
   },
   text: {
     fontSize: 24,
