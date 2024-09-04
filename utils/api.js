60233ddc0e1bf9a8875d910d85b60e2f2e8953d9
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-04 21:52:44 +0430

    add apis

--- utils/api.js
+++ utils/api.js
@@ -14,37 +14,58 @@
 
 //similar movies
 
-import axios from "axios"
+import axios from "axios";
 
-const API_KEY = "apikey"
+const API_KEY = "0ccd6fe236be70e53344b0810f4b34cb";
 
-const BASE_URL="https://api.themoviedb.org/3"
+const BASE_URL = "https://api.themoviedb.org/3";
 
-const api= axios.create({
-    baseURL: BASE_URL,
-    params:{
-        api_key:API_KEY,
-    },
+const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    api_key: API_KEY,
+  },
 });
 
 //fetching popular movies
-export const fetchPopularsMovies= async()=>{
-    try{
-        const response = await api.get("/movies/popular");
-        return response.data
-    }catch(error){
-        console.error(error);
-        throw error
-    }
-}
+export const fetchPopularsMovies = async () => {
+  try {
+    const response = await api.get("/movie/popular");
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
 
 //fetching popular tv show
-export const fetchPopularsTVShow= async()=>{
-    try{
-        const response = await api.get("/tv/popular");
-        return response.data
-    }catch(error){
-        console.error(error);
-        throw error
-    }
-}
+export const fetchPopularsTVShow = async () => {
+  try {
+    const response = await api.get("/tv/popular");
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+//fetching recomended movies
+export const fetchRecommendedMovies = async () => {
+  try {
+    const response = await api.get("/movie/top_rated");
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+//fetching recomended tv shows
+export const fetchRecommendedShows = async () => {
+  try {
+    const response = await api.get("/tv/top_rated");
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
