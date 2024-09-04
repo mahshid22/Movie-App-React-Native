aef13eefe06ca74c356b1a387614bb7acaa5f078
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-04 21:53:05 +0430

    use apis

--- app/_tabs/index.js
+++ app/_tabs/index.js
@@ -1,49 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Dimensions } from "react-native";
-import { View, Text, StyleSheet,ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView } from "react-native";
 import Carousel from "../components/Carousel";
 import MovieCard from "../components/MovieCard";
 import TVShowCard from "../components/TVShowCard";
+import { fetchPopularsTVShow, fetchPopularsMovies } from "../../utils/api";
 
-sampleData =[
-  {
-    id:1,
-    title: "Star Wars: Episode IV - A New Hope",
-    
-    poster: "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg",
-    Ratings: "8"
-  },
-  {
-    id:2,
-    title: "B",
-    
-    poster: "https://m.media-amazon.com/images/M/MV5BZDhlZmRlNmMtYmMyYy00Zjg0LWI0ZmQtYzNiNWM5YTU4YTI3XkEyXkFqcGdeQXVyNjQ0NjY3MDY@._V1_SX300.jpg",
-    Ratings: "8"
-
-  },
-  {
-    id:3,
-    title: "C",
-    poster: "https://m.media-amazon.com/images/M/MV5BOTM5Njc5ZTYtYzk1OS00ZmIxLTlkOTAtZmE3MjBiNjQ4MWQyXkEyXkFqcGdeQXVyNjIyNDgwMzM@._V1_SX300.jpg",
-    Ratings: "8"
-
-  }
-]
-
-const renderMovieItem=({item})=>(
-  <MovieCard movie={item}/>
-)
-const renderTVShowItem=({item})=>(
-  <TVShowCard show={item}/>
-)
+const renderMovieItem = ({ item }) => <MovieCard movie={item} />;
+const renderTVShowItem = ({ item }) => <TVShowCard show={item} />;
 const Home = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [popularTvShows, setPopularTvShows] = useState([]);
+  useEffect(() => {
+    const loacalDAta = async () => {
+      try {
+        const movies = await fetchPopularsMovies();
+        setPopularMovies(movies);
+        const tvShowa = await fetchPopularsTVShow();
+        setPopularTvShows(tvShowa);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    loacalDAta();
+    return () => {};
+  }, []);
+
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.text}>Hello from Homddde</Text>
       <Text style={styles.text}>Movies:</Text>
-      <Carousel data={sampleData} renderItem={renderMovieItem}/>
+      <Carousel data={popularMovies} renderItem={renderMovieItem} />
       <Text style={styles.text}>TV shows:</Text>
-      <Carousel data={sampleData} renderItem={renderTVShowItem}/>
+      <Carousel data={popularTvShows} renderItem={renderTVShowItem} />
     </ScrollView>
   );
 };
@@ -54,21 +43,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 10,
-    marginTop: 50
+    marginTop: 50,
   },
   text: {
     fontSize: 24,
     fontWeight: "700",
+    textAlign: "left",
   },
-  item:{
-    width: Dimensions.get("window").width*0.75,
+  item: {
+    width: Dimensions.get("window").width * 0.75,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#e0e0e0",
     borderRadius: 8,
     padding: 20,
     marginHorizontal: 8,
-    height: Dimensions.get("window").width*0.75
+    height: Dimensions.get("window").width * 0.75,
   },
-  
 });
