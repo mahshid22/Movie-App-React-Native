c5cf9e5ecb4078e696e6686b3066f78a8248d045
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-05 19:03:47 +0430

    clean movie cars component

--- app/components/MovieCard.js
+++ app/components/MovieCard.js
@@ -1,37 +1,29 @@
-import React from 'react';
-import { Card, Title, Paragraph } from 'react-native-paper';
-import { TouchableOpacity, StyleSheet, View } from 'react-native';
-import { useTheme } from '../../context/ThemeContext'; // Import the useTheme hook
+import React from "react";
+import { Card, Title, Paragraph } from "react-native-paper";
+import { StyleSheet, View, Pressable } from "react-native";
+import { useTheme } from "../../context/ThemeContext";
 
 const MovieCard = ({ movie, onPress }) => {
   const { theme } = useTheme(); // Access the current theme
 
   return (
-    <TouchableOpacity onPress={onPress} style={styles.cardContainer}>
+    <Pressable onPress={onPress} style={styles.cardContainer}>
       <View style={styles.shadowContainer}>
-        <Card
-        //   style={[
-        //     styles.card,
-        //     { backgroundColor: theme === 'dark' ? '#121212' : '#E8E8E8' },
-        //   ]}
-        >
+        <Card>
           <Card.Cover
-            source={{ uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`   }}
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+            }}
             style={styles.cover}
           />
           <Card.Content style={styles.content}>
-            <Title
-              style={[
-                styles.title,
-                { color: "red" },
-              ]}
-            >
+            <Title style={[styles.title, { color: "red" }]}>
               {movie.original_title}
             </Title>
             <Paragraph
               style={[
                 styles.paragraph,
-                { color: theme === 'dark' ? '#B0B0B0' : 'grey' }, // Apply paragraph color based on theme
+                { color: theme === "dark" ? "#B0B0B0" : "grey" },
               ]}
             >
               Rating: {movie.vote_average}
@@ -39,41 +31,41 @@ const MovieCard = ({ movie, onPress }) => {
           </Card.Content>
         </Card>
       </View>
-    </TouchableOpacity>
+    </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
   cardContainer: {
-    width: 150, // Fixed width
+    width: 150,
     margin: 8,
   },
   shadowContainer: {
     borderRadius: 8,
-    overflow: 'hidden',
-    elevation: 3, // Adjust shadow elevation
+    overflow: "hidden",
+    elevation: 3,
   },
   card: {
     flex: 1,
     borderRadius: 8,
-    width: 150, // Fixed width
-    height: 200, // Fixed height
+    width: 150,
+    height: 200,
   },
   cover: {
-    width: '100%',
-    height: 150, // Fixed height for the cover
+    width: "100%",
+    height: 150,
   },
   content: {
     paddingVertical: 8,
   },
   title: {
     fontSize: 16,
-    fontWeight: 'bold',
-    textAlign: 'center', // Centered text
+    fontWeight: "bold",
+    textAlign: "center",
   },
   paragraph: {
     fontSize: 14,
-    textAlign: 'center', // Centered text
+    textAlign: "center",
   },
 });
 
